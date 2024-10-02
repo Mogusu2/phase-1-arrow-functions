d039351cdba10352639980f82d4249c50feb4706
James Maroko <jamiemaroko41@gmail.com>
2024-10-02 11:33:41 +0300

    complete arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
+
+// Example Usage:
+console.log(divide()); // 20
+console.log(square(89)); // 16
+console.log(add(41, 96)); // 8
